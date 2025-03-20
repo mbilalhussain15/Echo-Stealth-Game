b655a0cd2947e5065f29f35be13e04092b921a27
Muhammad Bilal Hussain <mbilalhussain8473@gmail.com>
2025-03-20 13:27:14 +0100

    Enhance StoryLog functionality with data fragment placement and message decryption

--- Assets/Scripts/StoryLog.cs
+++ Assets/Scripts/StoryLog.cs
@@ -1,63 +1,201 @@
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using Random = UnityEngine.Random;
+
+using UnityEngine.SceneManagement;
+
 
 public class StoryLog : MonoBehaviour
 {
-    public string encryptedMessage = "01101000 01101001 01100100 01100100 01100101 01101110";
+
+    public StoryData storyData;
     private bool isNear = false;
+    private bool isCollected = false; 
     public GameObject messagePanel;
-    [SerializeField] private Text messageText;
+    [SerializeField] public Text messageText;
+    [SerializeField] public Button continueButton;
+    private int currentMessageIndex = 0;
+    private static StoryLog currentFragment;
+    private static int totalFragmentsPlaced = 0;
+
+    
+    public static void ResetFragmentCounter()
+    {
+        totalFragmentsPlaced = 0;
+    }
 
     void Start()
     {
+
         messagePanel.SetActive(false);
-        PlaceInFirstRow();
+
+
+        if (continueButton != null)
+        {
+            continueButton.onClick.AddListener(ContinueGame);
+        }
+        else
+        {
+            
+        }
+
+        if (transform.parent == null && totalFragmentsPlaced < 3)
+        {
+            PlaceDataFragments();
+        }
+
     }
-    void PlaceInFirstRow()
+
+    void PlaceDataFragments()
     {
         GridManager gridManager = FindFirstObjectByType<GridManager>();
-        if (gridManager == null) return;
+        if (gridManager == null)
+        {
+            return;
+        }
 
         ExitNode exitNode = FindFirstObjectByType<ExitNode>();
-        int exitNodeCol = exitNode.exitNodeColumn; 
+        if (exitNode == null)
+        {
+           
+            return;
+        }
+        else
+        {
+           
+        }
+        int exitNodeCol = exitNode.exitNodeColumn;
 
         List<Vector2> validPositions = new List<Vector2>();
 
-        for (int col = 0; col < gridManager.cols; col++)
+
+        int maxRows = Mathf.Min(4, gridManager.rows);
+
+        for (int row = 0; row < maxRows; row++)
         {
-            Tile tile = gridManager.grid[0, col];
-            if (tile != null && tile.isConnected && col != exitNodeCol)
+            for (int col = 0; col < gridManager.cols; col++)
             {
-                validPositions.Add(tile.transform.position);
+                Tile tile = gridManager.grid[row, col];
+                if (tile != null && tile.isConnected && col != exitNodeCol)
+                {
+                    validPositions.Add(tile.transform.position);
+                }
             }
         }
 
-        if (validPositions.Count > 0)
+        validPositions = ShuffleList(validPositions);
+        List<string> shuffledStories = new List<string>(storyData.encryptedMessages);
+        shuffledStories = ShuffleStringList(shuffledStories);
+
+        for (int i = 0; i < Mathf.Min(3, validPositions.Count); i++)
         {
-            transform.position = validPositions[Random.Range(0, validPositions.Count)];
+            if (totalFragmentsPlaced >= 3) break; 
+
+            GameObject newFragment = Instantiate(gameObject, validPositions[i], Quaternion.identity);
+            StoryLog newStoryLog = newFragment.GetComponent<StoryLog>();
+            newStoryLog.storyData = storyData;
+            //newStoryLog.currentMessageIndex = i;
+            newStoryLog.currentMessageIndex = System.Array.IndexOf(storyData.encryptedMessages, shuffledStories[i]);
+            newStoryLog.enabled = true; 
+
+            totalFragmentsPlaced++; 
         }
+
+        gameObject.SetActive(false);
     }
+    List<string> ShuffleStringList(List<string> list)
+    {
+        for (int i = 0; i < list.Count; i++)
+        {
+            string temp = list[i];
+            int randomIndex = Random.Range(i, list.Count);
+            list[i] = list[randomIndex];
+            list[randomIndex] = temp;
+        }
+        return list;
+    }
+    List<Vector2> ShuffleList(List<Vector2> list)
+    {
+        for (int i = 0; i < list.Count; i++)
+        {
+            Vector2 temp = list[i];
+            int randomIndex = Random.Range(i, list.Count);
+            list[i] = list[randomIndex];
+            list[randomIndex] = temp;
+        }
+        return list;
+    }
+
     void Update()
     {
-        if (isNear && Input.GetKeyDown(KeyCode.E))
+        if (isNear && (Input.GetKeyDown(KeyCode.E)) && isCollected)
+        {
+            ShowMessage();
+            PauseGame();
+        }
+    }
+
+    void PauseGame()
+    {
+        Time.timeScale = 0; 
+    }
+
+    void ShowMessage()
+    {
+
+        if (currentMessageIndex >= 0 && currentMessageIndex < storyData.encryptedMessages.Length)
         {
             messagePanel.SetActive(true);
-            messageText.text = "Decryption in progress...\n" + DecodeMessage(encryptedMessage);
+            messageText.text = "Decryption in progress...\n" + DecodeMessage(storyData.encryptedMessages[currentMessageIndex]);
+            currentFragment = this;
         }
+        else
+        {
+          
+        }
+    }
+
+    public void ContinueGame()
+    {
+        messagePanel.SetActive(false);
+        Time.timeScale = 1; 
+                         
+        if (currentFragment != null)
+        {
+            currentFragment.RemoveFragment();
+            currentFragment = null; 
+        }
+    }
+
+    private IEnumerator RemoveFragmentAfterDelay(float delay)
+    {
+        yield return new WaitForSecondsRealtime(delay);
+        RemoveFragment();
     }
 
     string DecodeMessage(string binaryCode)
     {
-        return "The system is watching you...";
+        string[] binaryWords = binaryCode.Split(' ');
+        string decodedMessage = "";
+        foreach (string word in binaryWords)
+        {
+            int asciiValue = Convert.ToInt32(word, 2);
+            decodedMessage += (char)asciiValue;
+        }
+        return decodedMessage;
     }
 
     void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.CompareTag("Player"))
+        if (other.CompareTag("Player") && !isCollected)
         {
             isNear = true;
+            isCollected = true; 
             FindFirstObjectByType<EndingManager>().CollectDataFragment();
+            StartCoroutine(RemoveFragmentAfterDelay(6));
         }
     }
 
@@ -66,7 +204,12 @@ public class StoryLog : MonoBehaviour
         if (other.CompareTag("Player"))
         {
             isNear = false;
-            messagePanel.SetActive(false);
         }
+    }
+
+    void RemoveFragment()
+    {
+       
+        gameObject.SetActive(false);
     }
 }
